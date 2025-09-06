1bad6cc1830fb370e10806b0fd6cbed02504b9f0
Kusal Lamsal <prastutlamsal41@gmail.com>
2025-09-06 21:08:42 +0545

    feat: add metadata generation for Create Product page

--- src/app/(app)/dashboard/products/create/page.tsx
+++ src/app/(app)/dashboard/products/create/page.tsx
@@ -1,6 +1,14 @@
 import prisma from "@/lib/db";
 import { ProductForm } from "./product-form";
 
+// add page metadata
+export async function generateMetadata() {
+  return {
+    title: "Create Product | Nature & Nurtures",
+    description: "Create a new product for your store",
+  };
+}
+
 export default async function CreateProductPage() {
   const categories = await prisma.category.findMany();
 
