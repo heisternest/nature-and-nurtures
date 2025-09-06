5b3243cb2cdf9800a2a0d7c3c57ee0dfa111d204
Kusal Lamsal <prastutlamsal41@gmail.com>
2025-09-06 21:34:56 +0545

    feat: add revalidation and cache settings to FAQ and Hero sections

--- src/components/shared/faq/index.tsx
+++ src/components/shared/faq/index.tsx
@@ -1,6 +1,9 @@
 import prisma from "@/lib/db";
 import { FAQSectionClient } from "./faq-client";
+export const revalidate = 0;
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export async function FAQSection() {
   const faqData = await prisma.pageContent.findUnique({
     where: { pageType: "FAQ" },
--- src/components/shared/hero/index.tsx
+++ src/components/shared/hero/index.tsx
@@ -2,7 +2,10 @@
 
 import prisma from "@/lib/db";
 import HeroClient from "./hero-client";
+export const revalidate = 0;
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 export async function HeroSection() {
   const heroSection = await prisma.pageContent.findUnique({
     where: {
